c88ed1f162673b403fd75c880ce6747468c4062d
lwarode <lukas.warode@gmx.de>
2022-07-13 15:17:01 +0200

    open discourse theme

---
+++ R/theme_od.R
@@ -0,0 +1,32 @@
+theme_od <- function() {
+
+  # font <- "EB Garamond"
+  # font <- "CMU Serif"
+  # font <- "ComputerModern"
+  # font <- "Times"
+  # font <- "Source Sans Pro"
+
+  sysfonts::font_add_google("Source Sans Pro", "ssp")
+
+  # base theme
+  ggplot2::theme_linedraw() %+replace%
+
+    ggplot2::theme(
+
+      text = element_text(family = "ssp"),
+      legend.text = element_text(size = 6),
+      legend.title = element_blank(),
+      legend.box.background = element_rect(colour = "black", fill = "white", linetype = "solid"),
+      # legend.position = c(0.85, 0.85),
+
+      # grid lines
+      panel.grid.major = element_line(color = "grey60", size = 0.2),
+      panel.grid.minor = element_line(color = "grey80", size = 0.1),
+
+      # faceting
+      strip.background = element_blank(),
+      strip.text = element_text(color = "black")
+
+    )
+
+}
