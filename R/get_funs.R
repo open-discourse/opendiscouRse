b8dbc68cfca09b17b55479ec3f2037d5d3dfd917
lwarode <lukas.warode@gmx.de>
2022-07-14 20:24:41 +0200

    age for lp start date

--- R/get_funs.R
+++ R/get_funs.R
@@ -1,12 +1,16 @@
-get_age <- function(date, round_val = 0) {
+get_age <- function(date, round_val = 2) {
   if (!class(date) == "Date") {
     stop("Select a valid variable of class 'Date'.")
   }
   round(as.numeric(Sys.Date() - date) / 365, round_val)
 }
 
-get_profession_group <- function() {data, var} {
-  jobs <- rjson::fromJSON(file = "data/jobs.json")
+# same but for start of lp
+get_age_lp <- function(lp_start_date, date_birth, round_val = 2) {
+  if (all(!class(lp_start_date) == "Date" | !class(date_birth) == "Date")) {
+    stop("Select a valid variable of class 'Date'.")
+  }
+  round(as.numeric(lp_start_date - date_birth) / 365, round_val)
 }
 
 get_lps <- function(data, input_id, output_format = "data.frame") {
@@ -25,3 +29,9 @@ get_lps <- function(data, input_id, output_format = "data.frame") {
       dplyr::pull()
   }
 }
+
+get_profession_group <- function() {data, var} {
+  jobs <- rjson::fromJSON(file = "data/jobs.json")
+}
+
+
