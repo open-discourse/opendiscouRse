f952ec70f02a3508b0188e1aa2115d4ac42de9d0
lwarode <lukas.warode@gmx.de>
2022-07-28 16:36:58 +0200

    na plot coverage function

--- R/plot_funs.R
+++ R/plot_funs.R
@@ -28,3 +28,54 @@ plot_dist <- function(data, metric_var, group_var, plot_type = "ridge_plot") {
       ggplot2::geom_boxplot()
   }
 }
+
+plot_cov <- function(data, group_var = NULL, exclude_vars = NULL) {
+  checkmate::check_data_frame(data)
+
+  if (is.null(group_var)) {
+    data %>%
+      dplyr::select(-exclude_vars) %>%
+      suppressMessages() %>%
+      purrr::map_df(~ sum(is.na(.))) %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(), ~ . / nrow(data))) %>%
+      tidyr::pivot_longer(everything()) %>%
+      ggplot2::ggplot(ggplot2::aes(x = name, y = value)) +
+      ggplot2::geom_col() +
+      ggplot2::scale_y_continuous(
+        name = "Percentage\nMissing",
+        limits = c(0, 1),
+        breaks = seq(0, 1, 0.2),
+        labels = scales::label_percent()
+      ) +
+      ggplot2::labs(x = "", y = "")
+  } else {
+    df <- data %>%
+      dplyr::select(-exclude_vars) %>%
+      dplyr::group_by(dplyr::across(group_var)) %>%
+      tidyr::nest() %>%
+      dplyr::mutate(
+        n = purrr::map(data, ~ dplyr::count(.)) %>% unlist(),
+        data = purrr::map(data, ~ purrr::map_df(., ~ sum(is.na(.))))
+      ) %>%
+      tidyr::unnest() %>%
+      suppressWarnings() %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(), ~ . / n)) %>%
+      tidyr::pivot_longer(- {{ group_var }} )
+
+    group_var <- rlang::sym(group_var)
+
+    df %>%
+      ggplot2::ggplot(ggplot2::aes(x = name, y = {{ group_var }}, fill = value)) +
+      ggplot2::geom_tile(color = "black", lwd = 0.5) +
+      ggplot2::scale_fill_continuous(
+        name = "Percentage\nMissing",
+        low = "white",
+        high = "black",
+        limits = c(0, 1),
+        breaks = seq(0, 1, 0.2),
+        labels = scales::label_percent()
+      ) +
+      ggplot2::coord_fixed() +
+      ggplot2::labs(x = "" , y = "")
+  }
+}
