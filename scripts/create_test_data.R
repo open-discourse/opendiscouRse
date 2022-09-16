6d72d57666c3284f6baa3ebab5eaac51d68abcdf
lwarode <lukas.warode@gmx.de>
2022-09-16 19:06:40 +0200

    test get_funs full

--- scripts/create_test_data.R
+++ scripts/create_test_data.R
@@ -18,15 +18,34 @@ create_test_file <- function(df) {
   )
 }
 
-# keep test data file sizes smaller than 1 mb
+### keep test data file sizes smaller than 1 mb ###
 
 # test data set just includes most recent electoral term and excludes data heavy columns
 test_speeches <- speeches %>%
-  select(-c(document_url, speech_content, search_speech_content)) %>%
-  sample_frac(0.01)
+  select(-c(document_url, search_speech_content)) %>%
+  sample_frac(0.0005)
   # filter(electoral_term == max(electoral_term))
 
 create_test_file(test_speeches)
 
 # no need to reduce politicians test file
 create_test_file(politicians)
+
+# create test contribution_simplified file based on speeches table
+test_contributions_simplified <- contributions_simplified %>%
+  # select(-content) %>%
+  filter(
+    as.numeric(speech_id) %in% as.numeric(test_speeches$id)
+  )
+
+create_test_file(test_contributions_simplified)
+
+# create test contribution_extended file based on speeches table
+test_contributions_extended <- contributions_extended %>%
+  # select(-content) %>%
+  filter(
+    as.numeric(speech_id) %in% as.numeric(test_speeches$id)
+  )
+
+create_test_file(test_contributions_extended)
+
--- tests/testthat/test-get_funs.R
+++ tests/testthat/test-get_funs.R
@@ -10,27 +10,6 @@ test_that("get_age_hist() returns numeric value", {
   expect_numeric(get_age_hist(Sys.Date(), Sys.Date()))
 })
 
-# get_faction_color() -----------------------------------------------------
-test_that("get_faction_color() returns consistent color codes", {
-  expect_true(
-    get_faction_color(
-      readr::read_csv(
-        test_path(
-          "data",
-          "faction_colors.csv"
-        )
-      ) %>%
-        dplyr::pull(faction_id)
-    ) %>%
-      na.omit() %>%
-      purrr::map(
-        ~ stringr::str_length(.x)
-      ) %>%
-      unique() %>%
-      length() == 1
-  )
-})
-
 # get_ets() ---------------------------------------------------------------
 test_that("get_ets() returns data.frame config 1", {
   expect_data_frame(
@@ -126,4 +105,136 @@ test_that("get_profession_groups() returns data.frame config 2", {
     )
   )
 })
+
+# get_faction_color() -----------------------------------------------------
+test_that("get_faction_color() returns consistent color codes", {
+  expect_true(
+    get_faction_color(
+      readr::read_csv(
+        test_path(
+          "data",
+          "faction_colors.csv"
+        )
+      ) %>%
+        dplyr::pull(faction_id)
+    ) %>%
+      na.omit() %>%
+      purrr::map(
+        ~ stringr::str_length(.x)
+      ) %>%
+      unique() %>%
+      length() == 1
+  )
+})
+
+# get_state() -------------------------------------------------------------
+test_that("get_state() returns a `character` vector", {
+  pol_vec <- readr::read_csv(
+      test_path(
+        "data",
+        "test_speeches.csv"
+      )
+    ) %>%
+    dplyr::pull(politician_id) %>%
+    as.character()
+
+  elec_vec <- readr::read_csv(
+    test_path(
+      "data",
+      "test_speeches.csv"
+    )
+  ) %>%
+    dplyr::pull(electoral_term) %>%
+    as.character()
+
+  expect_vector(
+    get_state(pol_vec, elec_vec)
+  )
+})
+
+test_that("get_state() can be used within dplyr::mutate()", {
+  expect_data_frame(
+    readr::read_csv(
+      test_path(
+        "data",
+        "test_speeches.csv"
+      )
+    ) %>%
+      dplyr::mutate(
+        state = get_state(politician_id, electoral_term)
+      )
+  )
+})
+
+# get_table_1() -----------------------------------------------------------
+test_that("get_table_1() returns `data.frame`", {
+  expect_data_frame(
+    get_table_1(
+      readr::read_csv(
+        test_path(
+          "data",
+          "test_speeches.csv"
+        )
+      ),
+      readr::read_csv(
+        test_path(
+          "data",
+          "test_contributions_simplified.csv"
+        )
+      ),
+      output_format = "data.frame"
+    )
+  )
+})
+
+test_that("get_table_1() returns `knitr_kable`", {
+  expect_class(
+    get_table_1(
+      readr::read_csv(
+        test_path(
+          "data",
+          "test_speeches.csv"
+        )
+      ),
+      readr::read_csv(
+        test_path(
+          "data",
+          "test_contributions_simplified.csv"
+        )
+      ),
+      output_format = "latex"
+    ),
+    "knitr_kable"
+  )
+})
+
+# get_implausible_data() --------------------------------------------------
+test_that("get_implausible_data() returns `data.frame` config 1", {
+  speeches <- readr::read_csv(
+      test_path(
+        "data",
+        "test_speeches.csv"
+      )
+    )
+
+  expect_data_frame(
+    get_implausible_data(speeches)
+  )
+})
+
+test_that("get_implausible_data() returns `data.frame` config 2", {
+  contributions_extended <- readr::read_csv(
+    test_path(
+      "data",
+      "test_contributions_extended.csv"
+    )
+  )
+
+  expect_data_frame(
+    get_implausible_data(contributions_extended)
+  )
+})
+
+
+
 
