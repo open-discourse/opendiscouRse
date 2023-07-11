2ff17961f4a3a75ee8ebf3b0c39a9fc6aaaeb9ba
lwarode <lukas.warode@gmx.de>
2023-07-11 12:41:12 +0200

    faction_colors.csv -> R/sysdata.rda

--- R/get_funs.R
+++ R/get_funs.R
@@ -174,13 +174,6 @@ get_profession_groups <- function(data, var, merge = TRUE) {
 #' @export
 #'
 get_faction_color <- function(input_id, id_type = "number") {
-  faction_colors <- readr::read_csv(
-    system.file(
-      "data/faction_colors.csv",
-      package = "opendiscouRse"
-    )
-  ) %>%
-    suppressMessages()
   # faction_colors <- read.csv(
   #   system.file("data", "faction_colors.csv", package = "opendiscouRse")
   # )
